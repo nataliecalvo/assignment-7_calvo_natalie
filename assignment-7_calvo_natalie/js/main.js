658969bee63b53799aabb4951b852b7da19c5700
Natalie Calvo <nataliercalvo@gmail.com>
2018-07-16 01:01:59 -0500

    added javascript to the jQuery branch

--- assignment-7_calvo_natalie/js/main.js
+++ assignment-7_calvo_natalie/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+
+});
+
+$(document).ready(function() {
   var headings = $('h2');
   headings.text('I’m Natalie Calvo, and I’m learning jQuery!')
-		});
+});
+
+$(document).ready(function() {
+    var paragraphs = $('p')
+    $(paragraphs).css ({
+      'background-color': 'grey',
+      'color': 'blue',
+      'font-size': '27px',
+    })
+});
